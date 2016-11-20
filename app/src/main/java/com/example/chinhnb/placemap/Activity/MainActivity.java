346213b2dbc407137e0f2045a798030638250ee1
chinhnb <bachinh2105@gmail.com>
2016-11-20 15:20:06 +0700

    ok

--- app/src/main/java/com/example/chinhnb/placemap/Activity/MainActivity.java
+++ app/src/main/java/com/example/chinhnb/placemap/Activity/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.chinhnb.placemap.Activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
@@ -16,13 +18,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.chinhnb.placemap.Dialog.DialogInfoWindowMap;
+import com.example.chinhnb.placemap.Dialog.DialogSignin;
+import com.example.chinhnb.placemap.Fragment.MapFragment;
+import com.example.chinhnb.placemap.Fragment.MovieFragment;
+import com.example.chinhnb.placemap.Other.CircleTransform;
 import com.example.chinhnb.placemap.Utils.*;
-import com.example.chinhnb.placemap.Fragment.HomeFragment;
 import com.example.chinhnb.placemap.R;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     // index to identify current nav menu item
     public static int navItemIndex = 0;
@@ -37,6 +57,9 @@ public class MainActivity extends AppCompatActivity
 
     private DrawerLayout drawer;
     private FloatingActionButton fab;
+
+    private GoogleMap mMap;
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +91,16 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        View navHeader = navigationView.getHeaderView(0);
+        ImageView imgProfile = (ImageView) navHeader.findViewById(R.id.imageView);
+        // Loading profile image
+        Glide.with(this).load(R.drawable.ic_noavatar)
+                .crossFade()
+                .thumbnail(0.5f)
+                .bitmapTransform(new CircleTransform(this))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgProfile);
+
         if (savedInstanceState == null) {
             navItemIndex = 0;
             CURRENT_TAG = Const.TAG_HOME;
@@ -80,16 +113,10 @@ public class MainActivity extends AppCompatActivity
 
         if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
             drawer.closeDrawers();
-
-            // show or hide the fab button
             toggleFab();
             return;
         }
 
-        // Sometimes, when fragment has huge data, screen seems hanging
-        // when switching between navigation menus
-        // So using runnable, the fragment is loaded with cross fade effect
-        // This effect can be seen in GMail app
         Runnable mPendingRunnable = new Runnable() {
             @Override
             public void run() {
@@ -103,29 +130,27 @@ public class MainActivity extends AppCompatActivity
             }
         };
 
-        // If mPendingRunnable is not null, then add to the message queue
         if (mPendingRunnable != null) {
             mHandler.post(mPendingRunnable);
         }
 
-        // show or hide the fab button
         toggleFab();
-
-        //Closing drawer on item click
         drawer.closeDrawers();
 
-        // refresh toolbar menu
         invalidateOptionsMenu();
     }
 
     private Fragment getHomeFragment() {
         switch (navItemIndex) {
             case 0:
-                // home
-                HomeFragment homeFragment = new HomeFragment();
-                return homeFragment;
+                // map
+                return MapFragment.newInstance();
+            case 1:
+                // movie
+                MovieFragment movieFragment = new MovieFragment();
+                return movieFragment;
             default:
-                return new HomeFragment();
+                return new MovieFragment();
         }
     }
 
@@ -175,17 +200,20 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             navItemIndex = 0;
             CURRENT_TAG = Const.TAG_HOME;
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_movies) {
+            navItemIndex = 1;
+            CURRENT_TAG = Const.TAG_MOVIES;
+        } else if (id == R.id.nav_photos) {
             Intent intent=new Intent(this,MapActivity.class);
             //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_notifications) {
             Intent intent=new Intent(this,MovieActivity.class);
             startActivity(intent);
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
 //        } else if (id == R.id.nav_share) {
 //
@@ -197,6 +225,61 @@ public class MainActivity extends AppCompatActivity
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng HANOI = new LatLng(21.0227002, 105.801944);
+        Marker hanoi = mMap.addMarker(new MarkerOptions().position(HANOI).title("Marker in Hà Nội"));
+        hanoi.setTag(0);
+        hanoi.setDraggable(true);
+
+        mMap.setInfoWindowAdapter(new DialogInfoWindowMap(this));
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                Marker hanoi= mMap.addMarker(new MarkerOptions().position(latLng).title("Thêm địa chỉ"));
+                hanoi.setTag(0);
+                hanoi.setDraggable(true);
+                hanoi.showInfoWindow();
+            }
+        });
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                return false;
+            }
+        });
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                DialogFragment newFragment = new DialogSignin();
+                newFragment.show(getSupportFragmentManager(), "missiles");
+            }
+        });
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HANOI, 12));
+    }
+
+    //set map
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            if(mapFragment!=null) {
+                mapFragment.getMapAsync(this);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
     }
 
     // show or hide the fab
--- app/src/main/java/com/example/chinhnb/placemap/Activity/MapActivity.java
+++ app/src/main/java/com/example/chinhnb/placemap/Activity/MapActivity.java
@@ -46,9 +46,7 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMapLon
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -84,42 +82,6 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMapLon
         //makeText(this,marker.getTitle(), LENGTH_SHORT).show();
 
         return false;
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 
     @Override
