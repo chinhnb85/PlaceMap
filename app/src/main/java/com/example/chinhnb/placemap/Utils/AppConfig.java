dd8f4a7eb295c9deb344c13f2214b0e9c5fdb2a0
chinhnb <bachinh2105@gmail.com>
2017-02-24 22:30:26 +0700

    1

--- app/src/main/java/com/example/chinhnb/placemap/Utils/AppConfig.java
+++ app/src/main/java/com/example/chinhnb/placemap/Utils/AppConfig.java
@@ -7,12 +7,12 @@ package com.example.chinhnb.placemap.Utils;
 public class AppConfig {
     // API
     public static String URL_ROOT = "http://103.47.192.100:8888/";
-    public static String URL_LOGIN = "http://103.47.192.100:8888/api/accountApi/CheckLogin";
-    public static String URL_LIST_LOCALTION = "http://103.47.192.100:8888/api/localtionApi/GetListLocaltionByAccountId";
-    public static String URL_CHECK_LOCALTION = "http://103.47.192.100:8888/api/localtionApi/CheckedLocaltion";
-    public static String URL_GET_LOCALTION = "http://103.47.192.100:8888/api/localtionApi/ViewDetailLocaltion";
-    public static String URL_ADD_LOCALTION = "http://103.47.192.100:8888/api/localtionApi/AddNewLocaltion";
-    public static String URL_EDIT_LOCALTION = "http://103.47.192.100:8888/api/localtionApi/EditLocaltion";
-    public static String URL_UPLOAD_IMAGE = "http://103.47.192.100:8888/Handler/HandlerUpload.ashx";
-    public static String URL_AUTO_LOCALTION_USER = "http://103.47.192.100:8888/api/localtionApi/AddNewAccountPlace";
+    public static String URL_LOGIN = URL_ROOT+"api/accountApi/CheckLogin";
+    public static String URL_LIST_LOCALTION = URL_ROOT+"api/localtionApi/GetListLocaltionByAccountId";
+    public static String URL_CHECK_LOCALTION = URL_ROOT+"api/localtionApi/CheckedLocaltion";
+    public static String URL_GET_LOCALTION = URL_ROOT+"api/localtionApi/ViewDetailLocaltion";
+    public static String URL_ADD_LOCALTION = URL_ROOT+"api/localtionApi/AddNewLocaltion";
+    public static String URL_EDIT_LOCALTION = URL_ROOT+"api/localtionApi/EditLocaltion";
+    public static String URL_UPLOAD_IMAGE = URL_ROOT+"Handler/HandlerUpload.ashx";
+    public static String URL_AUTO_LOCALTION_USER = URL_ROOT+"api/localtionApi/AddNewAccountPlace";
 }
